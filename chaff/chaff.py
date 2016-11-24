0aa85960c4ae57c03f5b406bd98bfc39071a6387
torchhound <jcieslik@umail.iu.edu>
2016-11-24 17:00:45 -0500

    Error checking and a small correction for the api call

--- chaff/chaff.py
+++ chaff/chaff.py
@@ -8,12 +8,16 @@ def chaff():
 	payload = nameGen()
 	ab = anonBrowser(proxies = [])
 	while True:
-		ab.anonymize()
-		print("Searching for {}".format(payload))
-		response = ab.open("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&" + payload)
-		jsonOut = json.loads(response)
-		print("Status: {}".format(jsonOut["responseStatus"]))
-		print("Results: {}".format(jsonOut["responseData"]["results"]))
+		try:
+			ab.anonymize()
+			print("Searching for {}".format(payload))
+			response = ab.open("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + payload)
+			jsonOut = json.loads(response)
+			print("Status: {}".format(jsonOut["responseStatus"]))
+			print("Results: {}".format(jsonOut["responseData"]["results"]))
+		except Exception as e:
+			print(e)
+			pass
 
 def main():
 	chaff()
