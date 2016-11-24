abd8f3fdb2db55ad3a6df0a93dd1a68a13289469
torchhound <jcieslik@umail.iu.edu>
2016-11-24 17:43:59 -0500

    Switched to duckduckgo from google, json parsing needs work, random strings now (rarely) contain spaces

--- chaff/chaff.py
+++ chaff/chaff.py
@@ -1,18 +1,26 @@
 import random
 import requests
 import json
+import time
 from namegenchaff import nameGen
 
 def chaff():
 	while True:
+		sleepTime = random.randint(1, 30)
+		print("Sleeping for {}".format(sleepTime))
+		time.sleep(sleepTime)
 		try:
 			payload = nameGen()
 			print("Searching for {}".format(payload))
-			response = requests.get("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + payload)
-			#if response.status_code == 
-			#jsonOut = json.loads(response.text)
-			#print("Status: {}".format(jsonOut["responseStatus"]))
-			#print("Results: {}".format(jsonOut["responseData"]["results"]))
+			response = requests.get("http://api.duckduckgo.com/?q=" + payload + "&format=json&pretty=1")
+			#jsonOut = json.loads(response.content)
+			print("Status: {}".format(response.status_code))
+			#for x in jsonOut['RelatedTopics']:
+				#print(RelatedTopics['Result']
+			#for x in jsonOut['Results']:
+				#print(Results['Result']
+			#print("Results: {}".format(jsonOut["RelatedTopics"]["Result"]))
+			#print("Results: {}".format(jsonOut["Results"]["Result"]))
 			print(response.json())
 		except Exception as e:
 			print(e)
